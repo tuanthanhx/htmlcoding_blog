f4935ff14bc3aa9688eba23c8fd85b2b23289c0d
Tuan Thanh Ngo <tuanthanhx@gmail.com>
2025-03-30 23:39:11 +0700

    [upd]

--- src/app/_components/common-sidebar.tsx
+++ src/app/_components/common-sidebar.tsx
@@ -27,9 +27,9 @@ export function CommonSidebar() {
       <div className="bg-white shadow-[0_1px_1px_rgba(0,0,0,0.06)] p-6 mt-5">
         <h2 className="mb-6 text-2xl font-bold">Tags</h2>
         {allTags.length > 0 ? (
-          <ul className="space-y-2">
+          <ul className="space-y-2 m-[0_-0.5rem_0_0]">
             {allTags.map((tag) => (
-              <li key={tag}>
+              <li key={tag} className="inline-block mr-2">
                 <Link href={`/tags/${encodeURIComponent(tag)}`} className="text-blue-600 hover:text-blue-800 transition-colors">
                   #{tag}
                 </Link>
